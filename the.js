d55b3d7ece601b7ecfc585789809f8b41d404f82
NextGen45 <tommy48steele@gmail.com>
2021-10-02 22:37:29 -0500

    update

--- the.js
+++ the.js
@@ -11,3 +11,14 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 var stores = [];
 var grandTotalSales = 0;
 
+var tableArticle = document.getElementById('tableArticle');
+var salesTable = document.createElement('table');
+tableArticle.appendChild(salesTable);
+var tableHeader = document.createElement('thead');
+salesTable.appendChild(tableHeader);
+var headerRow = document.createElement('tr');
+var tableBody = document.createElement('tbody');
+
+
+
+
