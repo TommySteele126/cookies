ee43dcb3f14234aeb7735c6047d7635f36876f4d
NextGen45 <tommy48steele@gmail.com>
2021-10-02 22:34:38 -0500

    update

---
+++ the.js
@@ -0,0 +1,13 @@
+
+  
+'use strict';
+
+// rng function for generating random sales data
+function getRandom(max, min) {
+  return Math.random() * (max - min) + min;
+}
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var stores = [];
+var grandTotalSales = 0;
+
